654dd1ea427160005ffa00221ce7523c5fc3206d
louisenje <louisenje@gmail.com>
2020-03-02 12:10:45 +0300

    gotten ellements by class name and added an event listener in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@
  $(".card").hover(function () {
     $(this).children(".card-1").fadeToggle("fast", "linear");
     });
+
+    function ready() {
+        var removeCartItemButtons = document.getElementsByClassName('btn-success')
+        for (var i = 0; i < removeCartItemButtons.length; i++) {
+            var button = removeCartItemButtons[i]
+            button.addEventListener('click', removeCartItem)
+    }
+    
