261fdc2b95cb77416a317a6211d90ed1a3985c5c
louisenje <louisenje@gmail.com>
2020-03-03 10:19:30 +0300

    created a variable that gets element by the price of the item

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,32 @@
     $(this).children(".card-1").fadeToggle("fast", "linear");
     });
 
-    if (document.readyState == 'loading') {
+  /*  if (document.readyState == 'loading') {
         document.addEventListener('DOMContentLoaded', ready)
     } else {
         ready()
     }
+    */
 
-    function ready() {
+    
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
     console.log(removeCartItemButtons)
      for (var i = 0; i < removeCartItemButtons.length; i++) {
             var button = removeCartItemButtons[i]
             button.addEventListener('click', function(event){
-            var buttonClicked = event.target
-            buttonClicked.parentElement.parentElement.remove()
+              var buttonClicked = event.target
+              buttonClicked.parentElement.parentElement.remove()
 
             })
         } 
+    
     function updateCartTotal(){
         var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-
+        var cartRows=cartItemContainer.getElementsByClassName('cart-row')
+        for (var i = 0; i < removeCartItemButtons.length; i++) {
+            var cartRows = cartRows[i]
+            var priceElement=cartRows.getElementsByClassName('cart-price')[0]
+            var quantityElement= cartRows.getElementsByClassName('cart-quantity-input')[0]
+            
+      }
     }
